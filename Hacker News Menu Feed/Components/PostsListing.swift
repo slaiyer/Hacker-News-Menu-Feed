282f639085650d755676b51d9738c9e8beb68c52
slaiyer <1776970+slaiyer@users.noreply.github.com>
2025-08-27 00:52:01 +0530

    add button for opening multiple links

--- Hacker News Menu Feed/Components/PostsListing.swift
+++ Hacker News Menu Feed/Components/PostsListing.swift
@@ -1,5 +1,6 @@
 import Foundation
 import SwiftUI
+import AppKit
 
 struct PostsListing: View {
   var posts: [StoryFetchResponse]
@@ -9,35 +10,56 @@ struct PostsListing: View {
       Array(posts.enumerated()),
       id: \.element.id
     ) { _, post in
-      VStack(alignment: .leading, spacing: 4) {
-        if let url = post.url {
-          CustomLink(title: post.title ?? "", link: url)
-            .foregroundColor(.primary)
-            .help("\(post.title ?? "")\n\n\(url)")
-        } else {
-          Text(post.title ?? "")
-            .foregroundColor(.primary)
-            .help(post.title ?? "")
-        }
-
-        Link(destination: URL(string: "https://news.ycombinator.com/item?id=\(post.id)")!) {
-          HStack() {
-            Text("􀆇 \(post.score)")
-              .frame(minWidth: 50, alignment: .leading)
-
-            Text("􀌲 \(post.comments ?? 0)")
-              .frame(minWidth: 50, alignment: .leading)
+      HStack(alignment: .center) {
+        Button {
+          if let raw = post.url, let extURL = URL(string: raw) {
+            NSWorkspace.shared.open(extURL)
           }
-          .font(.system(size: 10))
-          .foregroundColor(.secondary)
+          let hnURL = URL(string: "https://news.ycombinator.com/item?id=\(post.id)")!
+          NSWorkspace.shared.open(hnURL)
+        } label: {
+          Text("􀉣")
+            .font(.system(size: 10))
+            .foregroundColor(.secondary)
         }
-        .onHover(perform: { hovering in
+        .buttonStyle(.plain)
+        .onHover { hovering in
           if hovering {
             NSCursor.pointingHand.push()
           } else {
             NSCursor.pop()
           }
-        })
+        }
+
+        VStack(alignment: .leading) {
+          if let url = post.url {
+            CustomLink(title: post.title ?? "", link: url)
+              .foregroundColor(.primary)
+              .help("\(post.title ?? "")\n\n\(url)")
+          } else {
+            Text(post.title ?? "")
+              .foregroundColor(.primary)
+              .help(post.title ?? "")
+          }
+
+          Link(destination: URL(string: "https://news.ycombinator.com/item?id=\(post.id)")!) {
+            HStack {
+              Text("􀆇 \(post.score)")
+                .frame(minWidth: 50, alignment: .leading)
+              Text("􀌲 \(post.comments ?? 0)")
+                .frame(minWidth: 50, alignment: .leading)
+            }
+            .font(.system(size: 10))
+            .foregroundColor(.secondary)
+          }
+          .onHover { hovering in
+            if hovering {
+              NSCursor.pointingHand.push()
+            } else {
+              NSCursor.pop()
+            }
+          }
+        }
       }
     }
   }
